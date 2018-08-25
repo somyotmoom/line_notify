4b1608d364b324bdd69f08a34845d833781ee590
Somyot Srihanon <somyot.srihanon@gmail.com>
2018-08-25 17:11:09 +0700

    Add files via upload

--- send.php
+++ send.php
@@ -1,12 +1,8 @@
 <?php
 define('API', 'https://notify-api.line.me/api/notify');
-define('TOKEN', 'C3fPBRAhyd0Z7wCKkJloBTUy7q7bBogo8QB6OAfrvW2'); // เปลี่ยนเป็น Token ของเรา
+define('TOKEN', $_POST['token']); //รับ Token จากหน้าผู้ใช้งาน user.php
 $message = array(
-    'message' => 'ทดสอบส่ง LINE Notify', // ข้อความที่จะส่ง
-    'stickerPackageId' => 1, // ส่งสติกเกอร์ไปด้วย
-    'stickerId' => 2, // ส่งสติกเกอร์ไปด้วย
-    'imageThumbnail' => 'https://www.itcomputec.com/notify/thumbnail.jpg', // ลิงค์รูปภาพ ขนาดไม่เกิน 240x240
-    'imageFullsize' => 'https://www.itcomputec.com/notify/fullsize.jpg', // ลิงค์รูปภาพ ขนาดไม่เกิน 1024x1024
+    'message' => 'ทดสอบส่ง LINE Notify Service', // ข้อความที่จะส่ง
 );
 line_notify(TOKEN, $message); // เรียกใช้ฟังก์ชั่น
 function line_notify($token, $message){
@@ -15,11 +11,11 @@ function line_notify($token, $message){
     $data = http_build_query($message, '', '&');
     $cURL = curl_init();
     curl_setopt( $cURL, CURLOPT_URL, API); 
-    curl_setopt( $cURL, CURLOPT_SSL_VERIFYHOST, 0); 
-    curl_setopt( $cURL, CURLOPT_SSL_VERIFYPEER, 0); 
-    curl_setopt( $cURL, CURLOPT_POST, 1); 
-    curl_setopt( $cURL, CURLOPT_POSTFIELDS, $data); 
-    curl_setopt( $cURL, CURLOPT_FOLLOWLOCATION, 1); 
+	curl_setopt( $cURL, CURLOPT_SSL_VERIFYHOST, 0); 
+	curl_setopt( $cURL, CURLOPT_SSL_VERIFYPEER, 0); 
+	curl_setopt( $cURL, CURLOPT_POST, 1); 
+	curl_setopt( $cURL, CURLOPT_POSTFIELDS, $data); 
+	curl_setopt( $cURL, CURLOPT_FOLLOWLOCATION, 1); 
     curl_setopt( $cURL, CURLOPT_HTTPHEADER, $header); 
     curl_setopt( $cURL, CURLOPT_RETURNTRANSFER, 1); 
     
